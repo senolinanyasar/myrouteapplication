1e91af13d6e139549df8d03c91468c25e537db91
senolinanyasar <18070006062@stu.yasar.edu.tr>
2024-11-12 01:13:47 +0000

    Some changes have been made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,60 @@
 
 import React from 'react';
 import './Footer.css';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
 
-const Footer = () =>{
+const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2024 Şirketiniz. Tüm hakları saklıdır.</p>
-            <p>Bizimle iletişime geçin: info@myrouteapp.com</p>
-            <div className="footer-links">
-              <a href="#privacy">Privacy Policy</a>
-              <a href="#terms">Terms of Service</a>
-             </div>
+            <div className='footer-container'>
+                {/* Logo ve Adres */}
+                <div className='footer-logo-section'>
+                    <h2 className='footer-logo'>MyRouteApp</h2>
+                    <p>Şirket Adres : İZMİR</p>
+                    <div className='social-icons'>
+                        <FaFacebook />
+                        <FaTwitter />
+                        <FaInstagram />
+                        <FaLinkedin />
+                        <FaYoutube />
+                    </div>
+                </div>
+
+                {/* Site Haritası */}
+                <div className="footer-links">
+                    <h3>Özellikler</h3>
+                    <ul>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                    </ul>
+                </div>
+
+                <div className="footer-links">
+                    <h3>Şirket</h3>
+                    <ul>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                        <li>-----------</li>
+                    </ul>
+                </div>
+
+                <div className="footer-links">
+                    <h3>Hakkımızda</h3>
+                    <ul>
+                        <li>Gizlilik Koşulları</li>
+                        <li>KVKK Aydınlatma Metni</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p>&copy; 2024 Tüm hakları saklıdır.</p>
+            </div>
         </footer>
     );
 };
