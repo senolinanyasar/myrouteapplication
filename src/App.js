19b890cbbbf31fe0e9e0f3dd6ebd7ad13b12bda4
mehmetmusab <20070006065@stu.yasar.edu.tr>
2024-12-10 23:55:14 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import HomePage from './components/HomePage';
 import PricingPage from './components/PricingPage';
 import TopBar from './components/TopBar';
 import Footer from './components/Footer';
-<<<<<<< HEAD
 import IndustriesPage from './components/IndustriesPage';
 import FeaturesPage from './components/FeaturesPage';
-=======
->>>>>>> 32f3b973a2e700d5e86e8571b9af5d664df9ef1c
 
 
 function App() {
@@ -20,12 +17,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/pricing' element={<PricingPage />} />
-<<<<<<< HEAD
         <Route path='/industries' element={<IndustriesPage />} />
         <Route path='/features' element={<FeaturesPage />} />
-=======
-        
->>>>>>> 32f3b973a2e700d5e86e8571b9af5d664df9ef1c
       </Routes>
       <Footer/>
     </Router>
